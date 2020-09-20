ac40734dec1affbc59c04b35db527a05393f96c9
somi <cindy960602@naver.com>
2020-09-20 19:17:18 +0900

    react

--- src/App.js
+++ src/App.js
@@ -1,49 +1,25 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-const likeIt = [
-  {
-    id: 1,
-    name: "flower",
-    color: "all",
-    rating: 3,
-  },
-  {
-    id: 2,
-    name: "sky",
-    color: "blue",
-    rating: 4.2,
-  },
-  {
-    id: 3,
-    name: "han-river",
-    color: "green",
-    rating: 3.9,
-  },
-];
-
-//Fav라는 컴포넌트
-function Fav({ name, col }) {
-  return (
-    <div>
-      <h1>I love {name}</h1>
-      <h3>It is {col}</h3>
-    </div>
-  );
-}
-
 // jsx의 문법은 html과 매우 유사
-// props,컴포넌트 개념 숙지
-//App은 부모 컴포넌트, Fav라는 자식 컴포넌트를 갖고있음
-function App() {
-  return (
-    <div>
-      {/* key,name,item은 Fav라는 컴포넌트의 props임 */}
-      {likeIt.map((item) => (
-        <Fav key={item.id} name={item.name} col={item.color} />
-      ))}
-    </div>
-  );
+// props,컴포넌트, 클래스 개념 숙지
+//App이라는 클래스는 React클래스의 컴포넌트들을 가짐
+class App extends React.Component {
+  state = {
+    isLoading: true,
+    movies: [],
+  };
+
+  componentDidMount() {
+    setTimeout(() => {
+      this.setState({ isLoading: false });
+    }, 6000);
+  }
+
+  render() {
+    const { isLoading } = this.state;
+    return <div>{isLoading ? "Loading" : "We are ready"}</div>;
+  }
 }
 
 export default App;
